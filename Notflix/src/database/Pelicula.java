805621a136f154fdecc31d49a123f4d97065dc04
Mikel Fuentes <mikelfuentes@opendeusto.es>
2019-11-13 12:28:31 +0100

    qw

---
+++ Notflix/src/database/Pelicula.java
@@ -0,0 +1,23 @@
+package database;
+
+public class Pelicula {
+	int id;
+	String nombre;
+	int año;
+	String director;
+	String archi;
+	String imagen;
+
+	public Pelicula(int id, String nombre, int año, String director, String archi, String imagen) {
+		this.id = id;
+		this.nombre = nombre;
+		this.año = año;
+		this.director = director;
+		this.archi = archi;
+		this.imagen = imagen;
+	}
+	
+	
+	
+}
+

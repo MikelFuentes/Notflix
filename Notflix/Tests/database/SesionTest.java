c7ac7cd47b3d148c32b447fc2cadcfac9d8eab05
sweos <atorsb@gmail.com>
2019-12-07 16:26:41 +0100

    Generate Test for (almost) everything The tests are blank, tho

---
+++ Notflix/Tests/database/SesionTest.java
@@ -0,0 +1,50 @@
+package database;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SesionTest {
+
+    private Sesion sesion;
+
+
+    @BeforeEach     //Hace el setup antes de cada funcion
+    void setUp() {
+
+    }
+
+    @AfterEach      //Hace el teardown desùes de cada funcion
+    void tearDown() {
+    }
+
+    @Test
+    void getCon() {
+    }
+
+    @Test
+    void crear() {
+    }
+
+    @Test
+    void buscar() {
+    }
+
+    @Test
+    void meter_peli() {
+    }
+
+    @Test
+    void buscar2() {
+    }
+
+    @Test
+    void buscarPorUnTag() {
+    }
+
+    @Test
+    void buscartaClaTa() {
+    }
+}
